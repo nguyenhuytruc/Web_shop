7d1c0c9d9bd3064dfe1b50394eb618847547dd9c
nguyenhuytruc <huytruc1201@gmail.com>
2023-06-05 10:48:22 +0700

    add logo

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="images/logo.png" type="image/png">
     <title>Shop - iPhone</title>
 </head>
 
